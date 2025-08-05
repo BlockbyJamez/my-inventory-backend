38649928c1aade7ab4b0402cd4e489eea150679d
BlockbyJamez <danny90628@gmail.com>
2025-08-05 10:01:43 +0800

    debug byteString

--- middleware/checkauth.js
+++ middleware/checkauth.js
@@ -1,6 +1,6 @@
 export function checkAdmin(req, res, next) {
   try {
-    const role = req.headers["x-role"];
+    const role = req.body.role || req.headers["x-role"];
     if (role !== "admin") {
       return res.status(403).json({ error: "只有管理員可執行此操作" });
     }
